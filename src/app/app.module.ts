522cf83dddc104b53a8f0447219c1992e659a867
raiflivium <liviu.miron@raiffeisen.ro>
2019-12-03 14:11:42 +0200

    9 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
 
 
 import { AppComponent } from './app.component';
