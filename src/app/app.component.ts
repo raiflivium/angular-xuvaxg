3510fe535cf6618845b71e24be8780c0220a74ac
raiflivium <liviu.miron@raiffeisen.ro>
2019-12-03 11:33:30 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  constructor(private router: Router) { }
+
+  clickMe() {
+    console.log("i do nothing!");
+  }
+
+  nextPage() {
+    this.router.navigate(['/other']);
+  }
 }
